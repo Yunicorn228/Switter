96704039cccb40698f9c2fe1fe7dc9fe322f306f
yunicorn228 <sheldonyuxu@gmail.com>
2020-03-24 23:29:46 -0400

    comment

--- src/components/MyFriends/index.js
+++ src/components/MyFriends/index.js
@@ -34,7 +34,7 @@ const MyFriends = ({ user }) => {
 				{friendArr &&
 					friendArr.map(friend => (
 						<div className='myfriends-list-content'>
-							<img src={friend.avatar} alt='friend' />
+							<img src={friend.avatar} alt='frien' />
 							<div className='myfriends-name'>{friend.lastName}</div>
 						</div>
 					))}
